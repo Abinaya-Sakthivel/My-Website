0ab54b2615e5c857d374c97bc9faef6c58edb3bf
Abinaya-Sakthivel <136240590+Abinaya-Sakthivel@users.noreply.github.com>
2023-10-18 16:52:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Add interactivity (e.g., smooth scrolling) with JavaScript
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
+    
+    smoothScrollLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
